4015e21320e739923ae7b757b92fbe8653fca31b
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-10-15 19:44:17 +0800

    Add files via upload

---
+++ day5_fungsi int and float.py
@@ -0,0 +1,12 @@
+#fungsi int dan float
+#seperti yang kita tau input itu hanya membaca dari keyboard sebagai type data string
+#int digunakan mengkonversi string ke integer
+#contonya
+
+number = int(input("enter the number:"))
+
+
+#float hampir sama dengan int, namun float mengkorversi string ke float ( bilangan dengan titik)
+#contonya
+height = float(input("masukkan tinggi badan anda dalam satuan meter"))
+print("your height is", height)
